8becc26885ec34ec20ab6dbb68def05146932839
Ray Ma <mawangyang@live.com>
2021-08-08 10:37:17 -0600

    nice

---
+++ fact.py
@@ -0,0 +1,17 @@
+# CMPT 145 Lab04: Version Control
+#
+
+def factorial (n):
+    """
+    Purpose :
+    Calculate the factorial of a non - negative integer
+    Pre - conditions :
+    n: non - negative integer
+    Return :
+    a non - negative integer
+    """
+
+    hahahah nihoaa
+    pass
+
+
