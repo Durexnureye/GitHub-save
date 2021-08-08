63d607d91b760cbc9e26ebc52c37872b6548332a
Ray Ma <mawangyang@live.com>
2021-08-08 11:05:35 -0600

    Coded up a recursive implementation of factorial .

---
+++ VSC/fact.py
@@ -0,0 +1,18 @@
+# CMPT 145 Lab04: Version Control
+#
+
+def factorial (n):
+    """
+    Purpose :
+    Calculate the factorial of a non - negative integer
+    Pre - conditions :
+    n: non - negative integer
+    Return :
+    a non - negative integer
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
